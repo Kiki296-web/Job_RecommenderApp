10ad29faa31bb0ec5a071c3c195c4c642be757ef
jonasnikitha <jonasnikitha@gmail.com>
2025-08-26 18:12:22 +0200

    Deploying site for testing

--- job_engine/job_engine/settings.py
+++ job_engine/job_engine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6om)it@1)8u(&wv@p(#t1d6p6@y8=a7*$)0no=@#k-16zz90_q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'job_engine.urls'
@@ -131,7 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
