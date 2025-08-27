c910e65e26270c955066ac511c34e7b3903e6cb1
Nikitha Jonas <jonasnikitha@gmail.com>
2025-08-27 20:42:38 +0200

    Update urls.py

--- job_engine/users/urls.py
+++ job_engine/users/urls.py
@@ -1,4 +1,3 @@
-# users/urls.py
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView, me
 
@@ -7,4 +6,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("me/", me, name="me"),
+    path('', views.user_list, name='user-list'), 
+    path('<int:id>/', views.user_detail, name='user-detail'),
 ]
