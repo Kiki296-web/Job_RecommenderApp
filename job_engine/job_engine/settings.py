d0407727cdf80149773deed6598d64b43067daa2
jonasnikitha <jonasnikitha@gmail.com>
2025-08-27 20:14:06 +0200

    Modified settings files still fro deployment

--- job_engine/job_engine/settings.py
+++ job_engine/job_engine/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6om)it@1)8u(&wv@p(#t1d6p6@y8=a7*$)0no=@#k-16zz90_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://job-recommenderapp.onrender.com"]
 
 
 # Application definition
