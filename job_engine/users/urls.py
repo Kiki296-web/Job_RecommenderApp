e919c54fe77788b39b57d0cd7f59319d29d57cb5
Nikitha Jonas <jonasnikitha@gmail.com>
2025-08-27 20:39:46 +0200

    Update urls.py
    Removed the urls

--- job_engine/users/urls.py
+++ job_engine/users/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .views import RegisterView, LoginView, ProfileView, me
 
 urlpatterns = [
-    path('', views.user_list, name='user-list'),  # handles GET /api/users/
-    path('<int:id>/', views.user_detail, name='user-detail'),  # handles /api/users/1/
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
