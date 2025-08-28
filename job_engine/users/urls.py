2465f631d7d28b3b79f67e7ca83ccff84e57c74b
Nikitha Jonas <jonasnikitha@gmail.com>
2025-08-28 09:13:09 +0200

    Update urls.py

--- job_engine/users/urls.py
+++ job_engine/users/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import RegisterView, LoginView, ProfileView, me
 
 urlpatterns = [
-    path('', views.user_list, name='user-list'), 
-    path('<int:id>/', views.user_detail, name='user-detail'),
+    # path('', views.user_list, name='user-list'), 
+    # path('<int:id>/', views.user_detail, name='user-detail'),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
