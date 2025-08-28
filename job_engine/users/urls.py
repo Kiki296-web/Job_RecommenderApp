a6ca29d4b01fc38ebbcca6b5b0f0f9e5d7b25cae
Nikitha Jonas <jonasnikitha@gmail.com>
2025-08-28 09:03:39 +0200

    Update urls.py
    Fix: added missing views import in users/urls.py

--- job_engine/users/urls.py
+++ job_engine/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from . import views
 from .views import RegisterView, LoginView, ProfileView, me
 
 urlpatterns = [
+    path('', views.user_list, name='user-list'), 
+    path('<int:id>/', views.user_detail, name='user-detail'),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("me/", me, name="me"),
-    path('', views.user_list, name='user-list'), 
-    path('<int:id>/', views.user_detail, name='user-detail'),
+    
 ]
