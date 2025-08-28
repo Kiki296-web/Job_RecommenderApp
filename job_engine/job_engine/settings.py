76ece57a984cba01bd563b19a7c68e704a0dd696
Nikitha Jonas <jonasnikitha@gmail.com>
2025-08-28 14:02:14 +0200

    Update settings.py
    Changed Db from MYSQL To Postgre

--- job_engine/job_engine/settings.py
+++ job_engine/job_engine/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,14 +89,8 @@ WSGI_APPLICATION = 'job_engine.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'job_db',
-        'USER': 'root',
-        'PASSWORD': 'KikiJay@2002',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")  # Reads from Render's environment variable
 }
 
 
