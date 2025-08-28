497792027369df4fc2117278355048d38f8cc49b
Nikitha Jonas <jonasnikitha@gmail.com>
2025-08-28 20:58:36 +0200

    Update settings.py

--- job_engine/job_engine/settings.py
+++ job_engine/job_engine/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         
-    ]
+    ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",  # 👈 browser login form
