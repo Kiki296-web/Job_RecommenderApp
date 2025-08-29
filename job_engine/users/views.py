6b6041a6580112b7facfa0a34263bb1bf29b4c5d
jonasnikitha <jonasnikitha@gmail.com>
2025-08-29 20:00:38 +0200

    Made chnages to my users model and added login template

--- job_engine/users/views.py
+++ job_engine/users/views.py
@@ -7,6 +7,7 @@ from .models import User
 from .serializers import RegisterSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 
 # Register new user
@@ -17,14 +18,21 @@ class RegisterView(generics.CreateAPIView):
 
 # Login (returns token)
 class LoginView(ObtainAuthToken):
+    def get(self, request, *args, **kwargs):
+        # Show a simple login form in the browser
+        return render(request, "users/login.html")
+
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({
-            "token": token.key,
-            "user_id": token.user_id,
-            "username": token.user.username
-        })
+        token = Token.objects.get(key=response.data["token"])
+        return Response(
+            {
+                "token": token.key,
+                "user_id": token.user_id,
+                "username": token.user.username,
+            },
+            status=status.HTTP_200_OK,
+        )
 
 # Profile (requires authentication)
 class ProfileView(generics.RetrieveUpdateAPIView):
