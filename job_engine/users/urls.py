9c577b67d31945a595d915d9bdae0c4b35ec0806
jonasnikitha <jonasnikitha@gmail.com>
2025-08-29 21:04:06 +0200

    Stillworking on the users models.py,views.py for login/register/profile pages as well as their html files

--- job_engine/users/urls.py
+++ job_engine/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import RegisterView, LoginView, ProfileView,LogoutView, me
+from .views import RegisterView, LoginView, ProfileView,LogoutView
 
 urlpatterns = [
     # path('', views.user_list, name='user-list'), 
